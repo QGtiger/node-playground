9493c32e073d655c4a3e6fdb562f4b5d0855f484
光鱼 <guang.yu@yingdao.com>
2024-08-19 22:20:21 +0800

    feat: vercel analytics 分析添加

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import NodePlayground from "./NodePlaygroud";
 import { SpeedInsights } from "@vercel/speed-insights/react";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
     <>
       <NodePlayground />
       <SpeedInsights />
+      <Analytics />
     </>
   );
 }
